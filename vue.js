8a50924abe40faf662805b40fafaefac18780dc9
mipawn <admin@mipawn.local>
2020-07-24 16:16:28 +0800

    7.23

--- vue.js
+++ vue.js
@@ -120,4 +120,29 @@ var capitalize = cached(function (str) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 })
 
+var hyphenateRE = /\B([A-Z])/g
+var hyphenate = cached(function (str) {
+  return str.replace(hyphenateRE, '-$1').toLowerCase()
+})
+
+function polyfillBind (fn, ctx) {
+  function boundFn (a) {
+    var l = arguments.length
+    return l
+      ? l > 1
+        ? fn.apply(ctx, arguments)
+        : fn.call(ctx, a)
+      : fn.call(ctx)
+  }
+  boundFn._length = fn.length
+  return boundFn
+}
+
+function nativeBind (fn, ctx) {
+  return fn.bind(ctx)
+}
+
+var bind = Function.prototype.bind
+  ? nativeBind
+  : polyfillBind;
 
